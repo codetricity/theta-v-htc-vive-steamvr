a85ef70dcb2664ef56e277bf31b51fbb0b2c643b
Craig Oda <craig@oppkey.com>
2018-04-09 19:41:11 -0700

    main assets

---
+++ Assets/Scripts/webCamDetect.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class webCamDetect : MonoBehaviour
+{
+
+	void Start()
+	{
+		WebCamDevice[] devices = WebCamTexture.devices;
+		Debug.Log("Number of web cams connected: " + devices.Length);
+		for (int i = 0; i < devices.Length; i++)
+		{
+			Debug.Log(i + " " + devices[i].name);
+		}
+		string camName = devices[5].name;
+		Debug.Log("The webcam name is " + camName);
+		Renderer rend = this.GetComponentInChildren<Renderer>();
+		WebCamTexture mycam = new WebCamTexture();
+
+		mycam.deviceName = camName;
+		rend.material.mainTexture = mycam;
+
+		mycam.Play();
+	}
+}
